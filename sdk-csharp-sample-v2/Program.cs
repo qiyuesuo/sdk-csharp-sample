a705dce986c728459984a07d6f57e93151a5716b
chenhao.zhang 张忱昊 <chenhao.zhang@qiyuesuo.com>
2019-08-30 11:33:25 +0800

    去除敏感信息

--- sdk-csharp-sample-v2/Program.cs
+++ sdk-csharp-sample-v2/Program.cs
@@ -6,9 +6,9 @@ namespace sdk_csharp_sample_v2
     {
         static void Main(string[] args)
         {
-            string accessSecret = "kAt5C7ibSuoZiM5ixOUP0NbjmSmkvX";
-            string accessToken = "v7Xb9KNogN";
-            string serverUrl = "https://openapi.qiyuesuo.me";   //测试环境：https://openapi.qiyuesuo.cn ; 正式环境：https://openapi.qiyuesuo.com
+            string accessSecret = "替换为开放平台SECRET";
+            string accessToken = "替换为开放平台TOKAN";
+            string serverUrl = "https://openapi.qiyuesuo.cn";   //测试环境：https://openapi.qiyuesuo.cn ; 正式环境：https://openapi.qiyuesuo.com
 
             SDKClient client = new SDKClient(accessToken, accessSecret, serverUrl);
 
@@ -16,10 +16,10 @@ namespace sdk_csharp_sample_v2
             // 利用业务分类配置进行合同发送
             //SendByCategory example = new SendByCategory();
             //example.Process(client);
-            
+
             SendByCodeSetting example = new SendByCodeSetting();
             example.Process(client);
-            
+
 
         }
     }
